2d811d3542af76cacd8e92c9de9d33bab1bec704
Guilherme Borges <guilhermerosasborges@gmail.com>
2025-12-02 13:36:36 +0000

    update to api v2.0.3

--- app.py
+++ app.py
@@ -2,37 +2,38 @@ import os
 
 from flask import Flask, request
 from flask_cors import CORS, cross_origin
+from stcp.routes import get_routes, get_route_directions, get_route_stops
+from stcp.stops import get_stop_real_time
+from stcp.util import get_stop_route_departures
 
-from stcp.api import get_lines, get_line_directions, get_line_stops, get_stop_real_times
-from stcp.util import stop_departures
 from src.stops import write_stops_file, read_stops_file, get_static_stop_data
 
 app = Flask(__name__)
 CORS(app, support_credentials=True)
 
 
-@app.route('/line')
+@app.route('/route')
 @cross_origin(supports_credentials=True)
-def all_lines():
-    return get_lines()
+def all_routes():
+    return get_routes()
 
 
-@app.route('/line/<line_code>')
+@app.route('/route/<route_slug>')
 @cross_origin(supports_credentials=True)
-def get_line(line_code: str):
-    line = []
-    directions = get_line_directions(line_code)
+def get_route(route_slug: str):
+    route = []
+    directions = get_route_directions(route_slug)
     for direction in directions:
-        direction['stops'] = get_line_stops(line_code, direction['direction_code'])
+        direction['stops'] = get_route_stops(route_slug, direction['direction_id'])
 
         # append detailed stop data
         for i, stop in enumerate(direction['stops']):
-            stop_code = stop['stop_code']
-            direction['stops'][i] = get_static_stop_data(stop_code)
+            stop_id = stop['stop_id']
+            direction['stops'][i] = get_static_stop_data(stop_id)
 
-        line.append(direction)
+        route.append(direction)
 
-    return line
+    return route
 
 
 @app.route('/stop')
@@ -46,28 +47,31 @@ def all_stops():
     return read_stops_file()
 
 
-@app.route('/stop/<stop_code>')
+@app.route('/stop/<stop_id>')
 @cross_origin(supports_credentials=True)
-def get_stop(stop_code: str):
+def get_stop(stop_id: str):
+    stop_id = stop_id.upper()
+
     static_only = request.args.get('static_only', False)
 
-    stop_data = get_static_stop_data(stop_code)
+    stop_data = get_static_stop_data(stop_id)
 
     if not static_only:
         # append the departures to their respective line
-        line_departures = stop_departures(stop_code, False)
+        line_departures = get_stop_route_departures(stop_id)
 
-        for line in stop_data['lines']:
-            line_code = line['line_code']
-            line['next'] = line_departures[line_code] if line_code in line_departures else []
+        for route in stop_data['routes']:
+            route_id = route['route_id']
+            route['next'] = line_departures[route_id] if route_id in line_departures else []
 
     return stop_data
 
 
-@app.route('/time/<stop_code>')
+@app.route('/time/<stop_id>')
 @cross_origin(supports_credentials=True)
-def get_timetable(stop_code: str):
-    return get_stop_real_times(stop_code, use_hash_cache=False)
+def get_timetable(stop_id: str):
+    stop_id = stop_id.upper()
+    return get_stop_real_time(stop_id)
 
 
 if __name__ == '__main__':
--- src/stops.py
+++ src/stops.py
@@ -1,15 +1,12 @@
 import json
 import os
 
-from stcp.api import get_stop_data
-from stcp.util import get_all_stops
+from stcp.stops import get_stop_data, get_stops
 
 
 def write_stops_file(filename='stops_data.json'):
-    from stcp.api import get_stop_data
-
     with open(filename, 'w') as file:
-        stop_data = [get_stop_data(stop_code) for stop_code in get_all_stops()]
+        stop_data = [get_stop_data(stop['stop_id']) for stop in get_stops()]
         json.dump(stop_data, file)
 
 
@@ -18,11 +15,14 @@ def read_stops_file(filename='stops_data.json'):
         return json.loads(file.read())
 
 
-def get_static_stop_data(stop_code):
+def get_static_stop_data(stop_id):
     if os.path.isfile('stops_data.json'):
         all_stops = read_stops_file()
-        for stop in all_stops:
-            if stop_code == stop['stop_code']:
-                return stop
+        try:
+            for stop in all_stops:
+                if stop_id == stop['stop_id']:
+                    return stop
+        except KeyError:
+            pass
 
-    return get_stop_data(stop_code)
+    return get_stop_data(stop_id)
